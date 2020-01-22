c81059bae8d4c0dddc6c78d05adb568c306ea831
Tigran Sarkisian <1731046@gmail.com>
2020-01-22 12:50:12 +0300

    Refactored build.gradle files

--- app/src/main/java/com/sarkisian/gh/MainActivity.kt
+++ app/src/main/java/com/sarkisian/gh/MainActivity.kt
@@ -3,6 +3,12 @@ package com.sarkisian.gh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * INFO
+ * https://github.com/andreabresolin/AndroidCoroutinesPlayground
+ * https://blog.elpassion.com/create-a-clean-code-app-with-kotlin-coroutines-and-android-architecture-components-f533b04b5431
+ * https://github.com/leoacevedo/weather-app
+ * */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
