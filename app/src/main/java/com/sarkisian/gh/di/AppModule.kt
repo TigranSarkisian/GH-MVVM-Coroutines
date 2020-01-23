0de26062f7ab2b28ec760351f39d4969c81a7927
Tigran Sarkisian <1731046@gmail.com>
2020-01-24 00:43:13 +0300

    Used viewModel instead of single in DI Koin

--- app/src/main/java/com/sarkisian/gh/di/AppModule.kt
+++ app/src/main/java/com/sarkisian/gh/di/AppModule.kt
@@ -3,12 +3,13 @@ package com.sarkisian.gh.di
 import com.sarkisian.gh.data.api.ApiFactory
 import com.sarkisian.gh.data.repository.UserRepository
 import com.sarkisian.gh.screens.UserViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 object AppModule {
 
     val viewModelModule = module {
-        single { UserViewModel(get()) }
+        viewModel { UserViewModel(get()) }
     }
 
     val apiModule = module {
