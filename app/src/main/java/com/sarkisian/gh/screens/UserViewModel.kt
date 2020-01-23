c33b268a2697c55a9ec450a8128dc58a43a8e1d3
Tigran Sarkisian <1731046@gmail.com>
2020-01-23 14:50:37 +0300

    Added viewmodel

---
+++ app/src/main/java/com/sarkisian/gh/screens/UserViewModel.kt
@@ -0,0 +1,32 @@
+package com.sarkisian.gh.screens
+
+import androidx.lifecycle.ViewModel
+import com.sarkisian.gh.data.model.GithubUser
+import com.sarkisian.gh.data.repository.UserRepository
+
+
+class UserViewModel(
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+    init {
+        // fetchData()
+    }
+
+    suspend fun getUsers(): List<GithubUser> {
+        return userRepository.loadUsers()
+    }
+
+    /*private fun fetchData() {
+        viewModelScope.launch {
+            try {
+                _loadingState.value = LoadingState.LOADING
+                userRepository.refresh()
+                _loadingState.value = LoadingState.LOADED
+            } catch (e: Exception) {
+                _loadingState.value = LoadingState.error(e.message)
+            }
+        }
+    }*/
+
+}
